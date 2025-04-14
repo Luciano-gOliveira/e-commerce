23f62dc8a5f502ffa03506b4308c238652d60e78
Luciano Oliveira <aulascursos66@gmail.com>
2025-04-14 17:06:56 -0300

    chore: add prisma adapter setup

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prismaClient } from "./prisma"
  
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prismaClient),
   providers: [],
 })
