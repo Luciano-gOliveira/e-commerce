760d49eb16b43bc3ffbc0c962d356a577162f8ab
Luciano Oliveira <aulascursos66@gmail.com>
2025-04-18 19:29:36 -0300

    feat: add first discount banner to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,18 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div></div>
-  )
+    <div className="p-5">
+      <Image
+        src="/banner-discount01.png"
+        className="h-auto w-full"
+        sizes="100vw"
+        alt="até 55% de desconto só esse mês"
+        width={0}
+        height={0}
+      />
+
+      
+    </div>
+  );
 }
