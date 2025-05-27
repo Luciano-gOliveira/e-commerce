0ffd478697ab31b0992b3cdf5ad4511e7e66b95c
Luciano Oliveira <aulascursos66@gmail.com>
2025-05-27 01:35:33 -0300

    feat: ensure header logo can be a clickable link

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,15 +1,20 @@
-import {ShoppingCartIcon } from "lucide-react";
+import { ShoppingCartIcon } from "lucide-react";
 import { Card } from "./ui/card";
 import MenuSheetButton from "./menu-sheet-button";
+import Link from "next/link";
 
 const Header = () => {
-    return ( 
-        <Card className="flex justify-between items-center p-4 rounded-t-none">
-            <MenuSheetButton/>
-            <h1 className="text-lg font-semibold"><span className="text-primary">Valente</span> Store</h1>
-            <ShoppingCartIcon/>
-        </Card>
-     );
-}
- 
+  return (
+    <Card className="flex items-center justify-between rounded-t-none p-4">
+      <MenuSheetButton />
+      <Link href="/">
+        <h1 className="text-lg font-semibold">
+          <span className="text-primary">Valente</span> Store
+        </h1>
+      </Link>
+      <ShoppingCartIcon />
+    </Card>
+  );
+};
+
 export default Header;
