6e836be0d2fd0d78c81085b3f6a68d1bb4d0a381
Luciano Oliveira <aulascursos66@gmail.com>
2025-06-13 21:19:20 -0300

    feat:  ensure category icons can be reutilizable

--- app/(home)/_components/categorie-item.tsx
+++ app/(home)/_components/categorie-item.tsx
@@ -1,6 +1,6 @@
 import { Badge } from "@/app/_components/ui/badge";
+import { categoryIcon } from "@/app/constants/category-icons";
 import { Category } from "@prisma/client";
-import { HeadphonesIcon, KeyboardIcon, MonitorIcon, MouseIcon, SpeakerIcon, SquareIcon } from "lucide-react";
 import Link from "next/link";
 
 interface CategoryItemProps {
@@ -8,20 +8,13 @@ interface CategoryItemProps {
 }
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
-    const categoryIcon = {
-        keyboards: <KeyboardIcon size={16}/>,
-        monitors: <MonitorIcon size={16}/>,
-        headphones: <HeadphonesIcon size={16}/>,
-        mousepads: <SquareIcon size={16}/>,
-        speakers: <SpeakerIcon size={16}/>,
-        mouses: <MouseIcon size={16}/>
-    }
+
   return (
     <Link href={`category/${category.slug}`}>
       <Badge variant="outline" className="py-3 flex justify-center items-center gap-2 rounded-lg">
-      {categoryIcon[category.slug as keyof typeof categoryIcon]}
-      <span className="font-semibild text-xs">{category.name}</span>
-    </Badge>
+        {categoryIcon[category.slug as keyof typeof categoryIcon]}
+        <span className="font-semibild text-xs">{category.name}</span>
+      </Badge>
     </Link>
     
   );
